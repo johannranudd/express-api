b49358e331c3bfb237da66bf6e90e86c5fead969
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-01-05 18:44:45 +0100

    adds beginning of api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-console.log("hello from node");
+const app = require("express")();
+const PORT = 5500;
+// const express = require("express");
+// const app = express();
+// const PORT = 5500;
+
+// app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`alive on ${PORT}`);
+});
+
+app.get(`/tshirt`, (req, res) => {
+  res.status(200);
+  //   console.log("ello");
+});
+
+// .send({
+//       tshirt: "red",
+//       size: "large",
+//     });
